afb331d60073191537d91d3f509b2c680a740277
julkip <julkip@neunerdhausen.de>
2024-04-04 23:32:51 +0200

    code with error

---
+++ player.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+
+func _enter_tree():
+    set_multiplayer_authority(name.to_int())
+
+
+func _ready():
+    global_position = Vector2(randi() % 1000 + 50, randi() % 500 + 50)
+
+
+func _process(_delta):
+    if not is_multiplayer_authority():
+        return
+    if Input.is_action_just_pressed("ui_accept"):
+        self.rotation = self.rotation + PI / 2
---
+++ world.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var player_scene = preload("res://player.tscn")
+var peer = ENetMultiplayerPeer.new()
+
+
+func _ready():
+    randomize()
+    multiplayer.peer_connected.connect(add_player)
+
+
+func _on_join_pressed():
+    peer.create_client("127.0.0.1", 1027)
+    multiplayer.multiplayer_peer = peer
+    $CanvasLayer.hide()
+
+
+func _on_host_pressed():
+    peer.create_server(1027)
+    multiplayer.multiplayer_peer = peer
+    add_player()
+    $CanvasLayer.hide()
+
+
+func add_player(id: int = 1):
+    var player = player_scene.instantiate()
+    player.name = str(id)
+    call_deferred("add_child", player)
